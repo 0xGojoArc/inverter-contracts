04a57d1c2ccebba2cf8146ec6e1bebcdb1047168
0xGojoArc <gojo-acc@proton.me>
2025-05-14 22:55:55 +0530

    Fix: Fixed solc compile issues

--- test/modules/logicModule/oracle/utils/OptimisiticOracleV3Mock.sol
+++ test/modules/logicModule/oracle/utils/OptimisiticOracleV3Mock.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: LGPL-3.0-only
 pragma solidity 0.8.23;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
 import {OptimisticOracleV3Interface} from
     "@lm/abstracts/oracleIntegrations/UMA_OptimisticOracleV3/optimistic-oracle-v3/interfaces/OptimisticOracleV3Interface.sol";
 
